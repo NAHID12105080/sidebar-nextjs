f46671c226040380cbdc001bd641a04c8ddcd9ed
NAHID12105080 <nahid151341@gmail.com>
2024-07-30 20:07:51 +0600

    Initial commit on initial-branch

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import {AuroraHero} from "@/components/Hero.tsx";
+import {AuroraHero} from "@/components/Hero";
 export default function Home() {
   return (
   <div>
